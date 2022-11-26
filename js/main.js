8469f0628777a4b2ef9dc992b9119f674656e041
Renard Bergson <renardrock@gmail.com>
2022-11-25 22:19:46 -0300

    feat: :sparkles: introduzindo ideia inicial de validação do formulário 1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,81 @@
 // ====================================================== VARIABLES =====================================================
+const $newsLetterForm = document.querySelector('#newsLetterForm')
+
 const $offerList = document.querySelector('.offerList')
 const $moreOffers = document.querySelector('.moreOffers')
 let offerHTML = ''
 
+// ============================================= NEWSLETTER FORM VALIDATION =============================================
+function formReset() {
+    const { name, email, cpf, genre } = $newsLetterForm
+
+    name.value = ''
+    email.value = ''
+    cpf.value = ''
+
+    genre.forEach(item => {
+        item.checked = false
+      })
+}
+
+$newsLetterForm.onsubmit = e => {
+    e.preventDefault()
+
+    let isThereAnError = false
+    
+    const { name, email, cpf, genre } = $newsLetterForm
+    
+    if (!name.value) {
+        isThereAnError = true
+        name.nextElementSibling.innerText = 'campo vazio!' 
+        name.classList.add('inputError')
+    } else { 
+        name.nextElementSibling.innerText = ''
+        name.classList.remove('inputError')
+    }
+
+    if (!email.value) {
+        isThereAnError = true
+        email.nextElementSibling.innerText = 'campo vazio!'
+        email.classList.add('inputError')
+    } else {
+        email.nextElementSibling.innerText = ''
+        email.classList.remove('inputError')
+    } 
+
+    if (!cpf.value) {
+        isThereAnError = true
+        cpf.nextElementSibling.innerText = 'campo vazio!'
+        cpf.classList.add('inputError')
+    } else {
+        cpf.nextElementSibling.innerText = ''
+        cpf.classList.remove('inputError') 
+    }
+
+    genre.forEach(item => {
+        const masc = genre.item(0)
+        const fem = genre.item(1)
+
+        if (masc.checked == false && fem.checked == false) {
+            isThereAnError = true
+            const label = item.nextElementSibling
+            label.classList.add('inputError')
+        } else {
+            const label = item.nextElementSibling
+            label.classList.remove('inputError')
+        }
+    })
+
+    if (!isThereAnError) {
+        $newsLetterForm.submit()
+    }
+}
+
 // =========================================== API REQUEST AND DATA CONSTRUCT ===========================================
-document.body.onload = request()
+document.body.onload = () => {
+    formReset()
+    request()
+} 
 
 function request(link) {
     if (!link) {
@@ -32,7 +103,7 @@ function dataShow(data) {
 
 
     for (let product of products) {
-        const { image, id, description, oldPrice, price, installments: {count, value}} = product
+        const { image, id, description, oldPrice, price, installments: {count, value} } = product
         
         offerHTML += `
             <li class="offer">
