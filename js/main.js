9181923d9be7e3866b4f00904a3565c39f2368c5
Renard Bergson <renardrock@gmail.com>
2022-11-26 19:18:09 -0300

    refactor: :recycle: corrigindo numeração dos produtos

--- js/main.js
+++ js/main.js
@@ -4,6 +4,78 @@ const $newsLetterForm = document.querySelector('#newsLetterForm')
 const $offerList = document.querySelector('.offerList')
 const $moreOffers = document.querySelector('.moreOffers')
 let offerHTML = ''
+let productID = 0
+
+// =========================================== API REQUEST AND DATA CONSTRUCT ===========================================
+document.body.onload = () => {
+    formReset()
+    request()
+} 
+
+function request(link) {
+    if (!link) {
+        fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1')
+        .then(getResponse)
+        .then(dataShow)
+        .catch(error)
+    } else {
+        fetch(`https://${link}`)
+        .then(getResponse)
+        .then(dataShow)
+        .catch(error)
+    }
+}
+
+function getResponse(response) {
+    if (response.status === 200) {
+        return response.json()
+    }
+}
+
+
+function dataShow(data) {
+    const { products, nextPage } = data
+
+    for (let product of products) {
+        const { image, id, description, oldPrice, price, installments: {count, value} } = product
+
+        productID++
+
+        offerHTML += `
+            <li class="offer">
+                <div class="thumbnail" style="background-image: url(${image});"></div>
+
+                <span class="name"> Produto número ${productID} </span>
+
+                <p class="description">
+                    ${description}
+                </p>
+
+                <span class="oldPrice">De: ${oldPrice} </span>
+
+                <h4 class="newPrice">Por: ${price} </h4>
+
+                <span class="installments">
+                    ou ${count}x de R$${value.toString().replace('.', ',')}
+                </span>
+
+                <button>Comprar</button>
+            </li>
+        `
+
+        $offerList.innerHTML = offerHTML
+        productID = productID
+    }
+    
+    $moreOffers.onclick = () => {
+        offerHTML = offerHTML
+        request(nextPage)
+    }
+}
+
+function error() {
+    console.log('ops, ocorreu um erro!')
+}
 
 // ============================================= NEWSLETTER FORM VALIDATION =============================================
 $newsLetterForm.onsubmit = e => {
@@ -96,73 +168,4 @@ function formReset() {
     genre.forEach(item => {
         item.checked = false
       })
-}
-
-// =========================================== API REQUEST AND DATA CONSTRUCT ===========================================
-document.body.onload = () => {
-    formReset()
-    request()
-} 
-
-function request(link) {
-    if (!link) {
-        fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1')
-        .then(getResponse)
-        .then(dataShow)
-        .catch(error)
-    } else {
-        fetch(`https://${link}`)
-        .then(getResponse)
-        .then(dataShow)
-        .catch(error)
-    }
-}
-
-function getResponse(response) {
-    if (response.status === 200) {
-        return response.json()
-    }
-}
-
-
-function dataShow(data) {
-    const { products, nextPage } = data
-
-
-    for (let product of products) {
-        const { image, id, description, oldPrice, price, installments: {count, value} } = product
-        
-        offerHTML += `
-            <li class="offer">
-                <div class="thumbnail" style="background-image: url(${image});"></div>
-
-                <span class="name"> Produto número ${id + 1} </span>
-
-                <p class="description">
-                    ${description}
-                </p>
-
-                <span class="oldPrice">De: ${oldPrice} </span>
-
-                <h4 class="newPrice">Por: ${price} </h4>
-
-                <span class="installments">
-                    ou ${count}x de R$${value.toString().replace('.', ',')}
-                </span>
-
-                <button>Comprar</button>
-            </li>
-        `
-
-        $offerList.innerHTML = offerHTML
-    }
-
-    $moreOffers.onclick = () => {
-        offerHTML = offerHTML
-        request(nextPage)
-    }
-}
-
-function error() {
-    console.log('ops, ocorreu um erro!')
 }
